df39d74200f505da0893fbed3d48e05723aa1488
watson <info@ibco.ltd>
2024-03-18 04:08:03 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ scp -r /Users/ib./ema/ib root@x162-43-22-125.static.xvps.ne.jp:/var/www/ib.
 # デプロイ後の後処理を実行する
 echo "Post-deployment tasks..."
 # 例: サーバー上で必要なコマンドを実行する
-ssh root@x162-43-22-125.static.xvps.ne.jp 'cd /var/www/ib. && bundle install && rake db:migrate && systemctl restart ib.'
+ssh root@x162-43-22-125.static.xvps.ne.jp 'cd /var/www/ib. && /Users/ib./.rbenv/shims/bundle install && rake db:migrate && systemctl restart ib.'
 
 echo "Deployment completed successfully."
