efc52638cf2148822640a0a793f9e04647d5d7dd
watson <info@ibco.ltd>
2024-03-17 17:45:28 +0900

    master

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,46 +4,26 @@ lock "~> 3.18.1"
 set :application, "ib."
 set :repo_url, "git@github.com:IB2209/ib.git"
 set :branch, 'main'
-set :deploy_to, '/home/rails/demo'
+set :deploy_to, '/var/www/ib.'
 set :linked_files, %w(config/master.key)
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :keep_releases, 5
 set :rbenv_ruby, '3.3.0'
 set :log_level, :debug
 set :default_env, {
-  rbenv_root: "/home/user/.rbenv",
-  path: "/home/user/.rbenv/bin:$PATH"
+  rbenv_root: "/Users/ib./.rbenv",
+  path: "/Users/ib./.rbenv/bin:$PATH"
 }
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+append :linked_files, "config/master.key"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
+  desc "Upload master key"
+  task :upload_master_key do
+    on roles(:app) do
+      upload!("/Users/ib.//ema/ib/config/master.key", "#{shared_path}/config/master.key")
+    end
+  end
+end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+after "deploy:check:linked_files", "deploy:upload_master_key"
