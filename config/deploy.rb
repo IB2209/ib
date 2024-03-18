5f2a0c560b69a531e1f8aa78ff993a48211eb38a
watson <info@ibco.ltd>
2024-03-18 18:26:25 +0900

    unicorn排除

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,38 +3,17 @@ lock "~> 3.18.1"
 
 set :application, "ib."
 set :repo_url, "git@github.com:IB2209/ib.git"
-set :branch, 'main'
+set :user, "deploy"
 set :deploy_to, '/var/www/ib.'
-set :linked_files, %w(config/master.key)
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
-set :keep_releases, 5
-set :rbenv_ruby, '3.3.0'
-set :log_level, :debug
-set :default_env, {
-  rbenv_root: "/home/rails/.rbenv",
-  path: "/root/.rbenv/shims:/root/.rbenv/plugins/ruby-build/bin:/root/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-}
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
+# 並列数
+set :bundle_jobs, 2
+
+# リリース間で共有するリソースのファイルパスを書く
 append :linked_files, "config/master.key"
-append :rbenv_map_bins, 'bundle'
 
-namespace :deploy do
-  desc "Upload master key"
-  task :upload_master_key do
-    on roles(:app) do
-      upload!("/Users/ib./ema/ib/config/master.key", "#{shared_path}/config/master.key")
-    end
-  end
-
-  desc "Restart unicorn"
-  task :restart_unicorn do
-    on roles(:app) do
-      within release_path do
-        execute :bundle, "exec unicorn -c config/unicorn.rb -E #{fetch(:stage)} -D"
-      end
-    end
-  end
-end
-
-after "deploy:check:linked_files", "deploy:upload_master_key"
-after "deploy:published", "deploy:restart_unicorn"
+# 各リリースが共通で読み込むディレクトリを設定する
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets",  '.bundle'
