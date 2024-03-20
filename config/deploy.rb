6b898ff902684bbfd2b0f999024b7203bc71106d
watson <info@ibco.ltd>
2024-03-20 12:06:56 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,24 +14,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :bundle_jobs, 2
 
 # リリース間で共有するリソースのファイルパスを書く
-append :linked_files, "config/database.yml", "config/master.key"
+append :linked_files, "config/master.key"
 
 # 各リリースが共通で読み込むディレクトリを設定する
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets",  '.bundle'
-
-# config/deploy.rb
-
-# データベースの設定
-set :database_config, -> { "#{shared_path}/config/database.yml" }
-
-# デプロイ時にデータベース設定ファイルを読み込む
-namespace :deploy do
-  desc 'Copy database.yml to the current release'
-  task :copy_database_config do
-    on roles(:app) do
-      execute :cp, fetch(:database_config), "#{release_path}/config/database.yml"
-    end
-  end
-end
-
-before 'deploy:starting', 'deploy:copy_database_config'
