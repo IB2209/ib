103714579d131f4c60047500acf9014e3793360b
watson <info@ibco.ltd>
2024-03-21 21:03:59 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,35 +1,32 @@
 # Pumaの設定ファイル
 
-#bind "unix://var/local/work/ib./tmp/sockets/puma.sock"
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-# 最大スレッド数と最小スレッド数を設定します
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+rails_env = ENV.fetch('RAILS_ENV') { 'development' }
+if rails_env == 'production'
+  # socket
+  bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
-# 本番環境の場合、ワーカー数をプロセッサの数に設定します
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
+  rails_root = Rails.root
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  stdout_redirect(
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
+  )
+  daemonize
+
+else
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port        ENV.fetch("PORT") { 3000 }
 end
 
-# 開発環境の場合、ワーカータイムアウトを設定します
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# Specifies the `environment` that Puma will run in.
+environment rails_env
 
-# Pumaがリクエストを受け取るためにリッスンするポートを指定します
-port ENV.fetch("PORT") { 3000 }
-
-# Pumaが実行される環境を指定します
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Pumaが使用するPIDファイルを指定します
+# Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# アプリケーションの事前読み込みを許可します
-preload_app!
-
-
-# Railsアプリケーションのコンフィグファイルを指定します
-app_dir = File.expand_path("../..", __FILE__)
-rackup DefaultRackup
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
